aae69e63c5df385b4c6d49538ae573243354125f
Andriy0 <andriy2152@gmail.com>
2022-01-15 22:46:05 +0200

    seeds: add functions to populate tables with records
    - table adapters with random records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def populate_vendors
+  %w[
+    TP-Link
+    ASUS
+    D-Link
+    NETGEAR
+    Blueshadow
+    HP
+  ].each do |name|
+    Vendor.create name: name
+  end
+end
+
+def populate_adapter_types
+  %w[
+    internal
+    external
+  ].each do |name|
+    AdapterType.create name: name
+  end
+end
+
+def populate_connection_types
+  %w[
+    wired
+    wireless
+    wired/wireless
+  ].each do |name|
+    ConnectionType.create name: name
+  end
+end
+
+def populate_adapters_random(n)
+  vendor_ids = Vendor.ids
+  adapter_type_ids = AdapterType.ids
+  connection_type_ids = ConnectionType.ids
+  
+  n.times do
+    name = Faker::Hipster.sentence word_count: 5
+    vendor = Vendor.find(vendor_ids.sample(1)).first
+    adapter_type = AdapterType.find(adapter_type_ids.sample(1)).first
+    connection_type = ConnectionType.find(connection_type_ids.sample(1)).first
+    Adapter.create name: name, vendor: vendor, adapter_type: adapter_type, connection_type: connection_type
+  end
+end
+
+
+# populate_vendor
+# populate_adapter_types
+# populate_connection_types
+# populate_adapters_random(30)
