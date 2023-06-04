aa2515e7c528503213d19b3b3239a60935cc70ff
Andriy0 <andriy2152@gmail.com>
2023-06-04 10:02:24 +0300

    Tweak search

--- app/models/adapter.rb
+++ app/models/adapter.rb
@@ -3,19 +3,22 @@ class Adapter < ApplicationRecord
   belongs_to :adapter_type
   belongs_to :connection_type
 
-  validates :name, presence: true, length: {minimum: 5}
+  validates :name, presence: true, length: { minimum: 5 }
   validates :vendor_id, presence: true
   validates :adapter_type_id, presence: true
   validates :connection_type_id, presence: true
 
-  scope :categorize, ->(vendor_id, adapter_type_id, connection_type_id) do
-    adapters = Adapter.all.includes(:vendor).includes(:adapter_type).includes(:connection_type)
-    if vendor_id and adapter_type_id and connection_type_id
-      adapters = Adapter.where(vendor_id: vendor_id,
-                               adapter_type_id: adapter_type_id,
-                               connection_type_id: connection_type_id)
+  scope :categorize, lambda { |vendor_id, adapter_type_id, connection_type_id|
+    adapters = Adapter.all.includes(:vendor, :adapter_type, :connection_type)
+    if vendor_id || adapter_type_id || connection_type_id
+      search_params = { vendor_id: vendor_id,
+                        adapter_type_id: adapter_type_id,
+                        connection_type_id: connection_type_id }
+                      .reject { |_key, val| val.blank? }
+
+      adapters = adapters.where search_params
     end
 
-    adapters = adapters.order(created_at: :desc)
-  end
+    adapters.order(created_at: :desc)
+  }
 end
